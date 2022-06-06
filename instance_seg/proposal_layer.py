949674a6fd0c8e1c4aaa6a5592589e8591c8365a
tanner <tanneraddington@gmail.com>
2022-06-06 15:25:31 -0700

    Finish of proposal_layer.py, functions to generate proposed class boxes.

--- instance_seg/proposal_layer.py
+++ instance_seg/proposal_layer.py
@@ -2,8 +2,10 @@
 The Goal of this script is to create functions that can generate the proposal
 boxes, and region of interest.
 '''
+import numpy as np
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 
 def apply_box_deltas(boxes, deltas):
     '''
@@ -47,4 +49,52 @@ def call_proposal_layer(inputs, proposal_count, nms_threshold, anchors, config=N
     '''
     The goal of this method is to introduce the proposal layer. This layer
     applies bounding box refinement to anchors. more edits here
+
+    Proposals in normalized coordinates [batch, rois, (y1, x1, y2, x2)]
     '''
+
+    inputs[0] = inputs[0].squeeze(0)
+    inputs[1] = inputs[1].squeeze(0)
+
+    # foregorund class confidence
+    scores = inputs[0][:,1]
+
+    # box deltas [bach, num_rois, 4]
+    deltas = inputs[1]
+    std = Variable(torch.from_numpy(np.reshape(config.RPN_BBOX_STD_DEV,[1,4])).float(), requires_grad=False)
+    if config.GPU_COUNT:
+        std = std.cuda()
+    deltas = deltas * std
+
+    # performance enhancement
+    pre_nms_lim = min(6000, anchors.size()[0])
+    scores, order = scores.sort(decending=True)
+    order = order[:pre_nms_lim]
+    scores = scores[:pre_nms_lim]
+    # potentially a place to update the repo for batch size > 1
+    deltas = deltas[order.data,:]
+    anchors = anchors[order.data, :]
+
+    # apply the boxes
+    boxes = apply_box_deltas(anchors, deltas)
+
+    # clip to fit in image
+    height, width = config.IMAGE_SHAPE[:2]
+    window = np.array([0,0,height,width]).astype(np.float32)
+    boxes = clip_boxes(boxes,window)
+
+    # normalize
+    norm = Variable(torch.from_numpy(np.array([height,width,height,width])).float(), requires_grad=False)
+    if config.GPU_COUNT:
+        norm = norm.cuda()
+    normalized_boxes = boxes / norm
+
+    #addition of batch dimension
+    normalized_boxes = normalized_boxes.unsqueeze(0)
+    return normalized_boxes
+
+
+
+
+
+
